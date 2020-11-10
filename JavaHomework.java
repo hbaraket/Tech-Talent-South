4a28823dc872692b8cb421c89110e30f415ffdff
hbaraket <hbaraket@uncc.edu>
2020-11-10 09:47:55 -0500

    Commit Java Homework 1

---
+++ JavaHomework.java
@@ -0,0 +1,16 @@
+/**
+ * JavaHomework
+ */
+public class JavaHomework {
+
+    public static void main(String[] args) {
+        int age = 24;
+        String name = "Hussein";
+
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+
+        int something;
+
+        System.out.println("Print something " + something);
+    }
+}
